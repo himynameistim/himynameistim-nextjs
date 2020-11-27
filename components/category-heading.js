82ab10c5efdc43cfb23268954e45d120ac3ba641
Timothy <Tim@Macintosh.local>
2020-11-27 08:04:51 +0000

    Category Heading

--- components/category-heading.js
+++ components/category-heading.js
@@ -1,10 +1,13 @@
 import React from "react"
 import categoryStyles from "../styles/category-heading.module.css"
+import Container from 'react-bootstrap/Container';
 
 export default function Category({ name }) {
   return (
-    <div className={categoryStyles.banner}>
-      { name }
+    <div className={["d-flex align-items-center", categoryStyles.banner].join(" ")}>
+        <Container>
+        { name }
+        </Container>
     </div>
   )
 }
