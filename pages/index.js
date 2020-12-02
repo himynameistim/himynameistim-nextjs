0394617e1c3c995d6860eed2b1ceb9a4dfc59533
Tim <tim@lab.co.uk>
2020-12-02 09:30:32 +0000

    Features articles layout

--- pages/index.js
+++ pages/index.js
@@ -28,18 +28,25 @@ export default function Home() {
   */}
 
 <div class="container mx-auto px-8">
-  <div class="flex flex-wrap">
-    <div class="w-full flex flex-col sm:w-1/2 lg:w-5/12">
-      <div class="flex-1 pr-8">
-      <FeaturedLink1 colspan="col-span-5" rowspan="row-span-2" category="Sitecore" title="HOW TO CREATE A CUSTOM DROPLIST IN SITECORE" date="October 4, 2020"></FeaturedLink1>
+  <div class="flex flex-wrap ">
+    <div class="w-full flex flex-col sm:w-1/2 lg:w-5/12 pr-4">
+      <div class="flex-1 grid place-items-end"  style={{ backgroundImage: "url(https://preview.colorlib.com/theme/eden/img/magazine/1.jpg)", minHeight: "400px" }}>
+        <div class="bg-white w-5/6 p-8">
+            <div className="text-highlight mb-1">Sitecore</div>
+            <div className="text-2xl font-semibold mb-2">HOW TO CREATE A CUSTOM DROPLIST IN SITECORE</div>
+            <div className="text-xs">October 4, 2020</div>
+        </div>
       </div>
     </div>
-    <div class="w-full flex flex-col sm:w-1/2 lg:w-4/12">
-      <div class="flex-1 pl-8 pr-8" style={{ backgroundImage: "url(https://preview.colorlib.com/theme/eden/img/magazine/1.jpg)" }}>
+    <div class="w-full flex flex-col sm:w-1/2 lg:w-4/12 pl-4 pr-4">
+      <div class="flex-1" style={{ backgroundImage: "url(https://preview.colorlib.com/theme/eden/img/magazine/1.jpg)" }}>
+        <div className="text-highlight mb-1 text-center max-w-prose">Dev Ops</div>
+        <div className="text-2xl font-semibold mb-2 text-center max-w-min mx-auto">WHAT ARE AUTOMATED DEPLOYMENTS AND WHY DO I NEED THEM?</div>
+        <div className="text-xs text-center max-w-prose">October 8, 2020</div>
       </div>
     </div>
-    <div class="w-full flex flex-col sm:w-1/2 lg:w-3/12">
-      <div class="flex-1 pl-8 flex flex-col">
+    <div class="w-full flex flex-col sm:w-1/2 lg:w-3/12 pl-4">
+      <div class="flex-1 flex flex-col">
           <div class="flex-1 mb-4" style={{ backgroundImage: "url(https://preview.colorlib.com/theme/eden/img/magazine/1.jpg)" }}>
 1
           </div>
