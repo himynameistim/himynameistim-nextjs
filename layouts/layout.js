c7345b66a5b58a0ad3ba82fae1197a68a68b6124
Timothy <Tim@Macintosh.local>
2020-12-04 08:34:22 +0000

    Update class to classname

--- layouts/layout.js
+++ layouts/layout.js
@@ -3,11 +3,12 @@ import Link from "next/link"
 import Prism from "prismjs"
 
 export default function Layout({children}) {
+    const [isMenuOpen, setMenuOpen] = useState(false)
+
     useEffect(() => {
         Prism.highlightAll();
     }, []);
     
-    const [isMenuOpen, setMenuOpen] = useState(false)
     return (
         <div>
         <header className="">
@@ -27,7 +28,7 @@ export default function Layout({children}) {
 
                         Menu open: "hidden", Menu closed: "block"
                     */}
-                    <svg class={`${isMenuOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <svg className={`${isMenuOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                     {/* Icon when menu is open.
@@ -35,7 +36,7 @@ export default function Layout({children}) {
 
                         Menu open: "block", Menu closed: "hidden"
                     */}
-                    <svg class={`${isMenuOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <svg className={`${isMenuOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                     </button>
