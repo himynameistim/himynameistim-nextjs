218b9b0493e7eddb43dc416804c293b0028a06b7
Tim <tim@lab.co.uk>
2020-12-23 17:40:43 +0000

    Removed debug code

--- pages/search.tsx
+++ pages/search.tsx
@@ -7,15 +7,13 @@ import algoliasearch from 'algoliasearch';
 import { GetServerSideProps} from 'next'
 
 
-export default function Search({ data, time, route }: { data: AlgoliaHits, time: string, route: string }) {
+export default function Search({ data }: { data: AlgoliaHits }) {
   return (
   <Layout>
   <Head>
     <title>Hi My Name Is Tim</title>
   </Head>
   <CategoryHeading name="Search"></CategoryHeading>
-  <h1>{time}</h1>
-  <h1>{route}</h1>
   {data.hits.map((hit) => (
     <h2>{hit.title}</h2>
   ))}
@@ -35,21 +33,18 @@ export type AlgoliaHit = {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   var content: AlgoliaHits;
-  var route: string;
   if (process.env.algoliaAppId && process.env.algoliaApiKey && context.query.s)
   {
-    route = "key found"
     const algoliaClient = algoliasearch(process.env.algoliaAppId, process.env.algoliaApiKey);
     const algoliaIndex = algoliaClient.initIndex('Posts')
 
     content = await algoliaIndex.search(context.query.s.toString());
   } else {
-    route = "else"
     content = {
       hits: []
     }
   }
   return {
-    props: { data: content, time: Date.now().toString(), route: route }, // will be passed to the page component as props
+    props: { data: content }, // will be passed to the page component as props
   }
 }
