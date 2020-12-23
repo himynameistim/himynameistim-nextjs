3e14f038b755937425ee996328ddc5856e7ba0a4
Tim <tim@lab.co.uk>
2020-12-23 17:31:13 +0000

    SSR test

--- pages/search.tsx
+++ pages/search.tsx
@@ -7,13 +7,14 @@ import algoliasearch from 'algoliasearch';
 import { GetServerSideProps} from 'next'
 
 
-export default function Search({ data }: { data: AlgoliaHits }) {
+export default function Search({ data, time }: { data: AlgoliaHits, time: string }) {
   return (
   <Layout>
   <Head>
     <title>Hi My Name Is Tim</title>
   </Head>
   <CategoryHeading name="Search"></CategoryHeading>
+  <h1>{time}</h1>
   {data.hits.map((hit) => (
     <h2>{hit.title}</h2>
   ))}
@@ -46,6 +47,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
   return {
-    props: { data: content}, // will be passed to the page component as props
+    props: { data: content, time: Date.now().toString() }, // will be passed to the page component as props
   }
 }
