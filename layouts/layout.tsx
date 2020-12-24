baa612c09f57edac903e8eb872ecd80be3ba08bf
Tim <tim@lab.co.uk>
2020-12-24 11:34:58 +0000

    Removed tailwind styles from layout

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -30,14 +30,14 @@ export default function Layout({children}: {
             <div className="logo">
               <div className="nav-button">
                   {/* Mobile menu button*/}
-                  <button onClick={() => setMenuOpen(!isMenuOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
-                  <span className="sr-only">Open main menu</span>
+                  <button onClick={() => setMenuOpen(!isMenuOpen)} aria-expanded="false">
+                  <span>Open main menu</span>
                   {/* Icon when menu is closed. 
                       Heroicon name: menu
 
                       Menu open: "hidden", Menu closed: "block"
                   */}
-                  <svg className={`${isMenuOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <svg className={`${isMenuOpen ? 'hidden' : 'block'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                   </svg>
                   {/* Icon when menu is open.
@@ -45,7 +45,7 @@ export default function Layout({children}: {
 
                       Menu open: "block", Menu closed: "hidden"
                   */}
-                  <svg className={`${isMenuOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <svg className={`${isMenuOpen ? 'block' : 'hidden'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                   </svg>
                   </button>
@@ -65,36 +65,26 @@ export default function Layout({children}: {
               </div>
             </div>
             <nav>
-            <div className="hidden sm:block max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex items-center justify-between h-20">
-                <div className="flex-1 flex items-center justify-center">
-                    <div className="hidden sm:block sm:ml-6">
-                    <div className="flex items-center justify-center space-x-4">
-                        <Link href="/"><a className="px-3 py-2 text-base font-medium">Home</a></Link>
-                        <Link href="/sitecore"><a className="px-3 py-2 text-base font-medium">Sitecore</a></Link>
-                        <Link href="/devops"><a className="px-3 py-2 text-base font-medium">Devops</a></Link>
-                        <Link href="/web-development"><a className="px-3 py-2 text-base font-medium">Web Development</a></Link>
-                        <Link href="/about"><a className="px-3 py-2 text-base font-medium">About</a></Link>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
+              <div>
+                  <Link href="/"><a>Home</a></Link>
+                  <Link href="/sitecore"><a>Sitecore</a></Link>
+                  <Link href="/devops"><a>Devops</a></Link>
+                  <Link href="/web-development"><a>Web Development</a></Link>
+                  <Link href="/about"><a>About</a></Link>
+              </div>
             {/*
             Mobile menu, toggle classes based on menu state.
 
             Menu open: "block", Menu closed: "hidden"
             */}
-            <div className={`${isMenuOpen ? 'block' : 'hidden'} sm:hidden`}>
-                <div className="px-2 pt-2 pb-3 space-y-1">
-                <Link href="/"><a className="block px-3 py-2 text-base font-medium">Home</a></Link>
-                <Link href="/sitecore"><a className="block px-3 py-2 text-base font-medium">Sitecore</a></Link>
-                <Link href="/devops"><a className="block px-3 py-2 text-base font-medium">Devops</a></Link>
-                <Link href="/web-development"><a className="block px-3 py-2 text-base font-medium">Web Development</a></Link>
-                <Link href="/about"><a className="block px-3 py-2 text-base font-medium">About</a></Link>
-                </div>
+            <div className={`${isMenuOpen ? 'block' : 'hidden'}`}>
+              <Link href="/"><a>Home</a></Link>
+              <Link href="/sitecore"><a>Sitecore</a></Link>
+              <Link href="/devops"><a>Devops</a></Link>
+              <Link href="/web-development"><a>Web Development</a></Link>
+              <Link href="/about"><a>About</a></Link>
             </div>
-            </nav>
+          </nav>
 
         </header>
         {children}
