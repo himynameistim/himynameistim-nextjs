fac51a8b7c80bfba439666c9935aa1c2845e766a
Tim <tim@lab.co.uk>
2020-12-24 12:21:16 +0000

    Removing tailwind from Featured

--- components/featured-row-1.tsx
+++ components/featured-row-1.tsx
@@ -11,7 +11,7 @@ export interface FeaturedRow1Model {
 
 export function FeaturedRow1({ posts }: {posts: FeaturedRow1Model}) {
     return (
-  <div className="container mx-auto mb-10">
+  <div className={[ "container",linkStyles.featuredRow].join(" ")}>
   <div className={[ linkStyles.row, "grid grid-cols-1 sm:grid-cols-2 md:grid-rows-2 md:grid-cols-12 grid-flow-row md:grid-flow-col gap-4"].join(" ")}>
     <Link href={linkResolver(posts[0])}>
     <a className={[ linkStyles.mainBlock, "md:col-span-5 md:row-span-2 pt-8 flex justify-center items-center grid place-items-end place-content-end bg-cover"].join(" ")} style={{ backgroundImage: "url(" + posts[0].image.url + "&fit=crop&max-w=630&max-h=400)" }}>
@@ -24,31 +24,31 @@ export function FeaturedRow1({ posts }: {posts: FeaturedRow1Model}) {
     </Link>
     { posts[1] && posts[1].image &&
     <Link href={linkResolver(posts[1])}>
-    <a className={[ linkStyles.secondBlock, "md:col-span-4 md:row-span-2 p-8 flex justify-center items-center bg-cover"].join(" ")} style={{ backgroundImage: "url(" + posts[1].image.url + "&fit=crop&max-w=500&max-h=400)" }}>
+    <a className={linkStyles.secondBlock} style={{ backgroundImage: "url(" + posts[1].image.url + "&fit=crop&max-w=500&max-h=400)" }}>
       <div>
-        <div className="text-white mb-1 text-center max-w-prose">{ posts[1].category}</div>
-        <div className="text-white text-2xl font-semibold mb-2 text-center max-w-min mx-auto">{ posts[1].title}</div>
-        <div className="text-white text-xs text-center max-w-prose">October 8, 2020</div>
+        <div>{ posts[1].category}</div>
+        <div>{ posts[1].title}</div>
+        <div>October 8, 2020</div>
       </div>
     </a>
     </Link>
     }
     { posts[2] && 
     <Link href={linkResolver(posts[2])}>
-    <a className={[ linkStyles.finalBlocks, "md:col-span-3 md:row-span-1 pt-8 flex justify-center items-center grid place-content-end bg-cover"].join(" ")} style={{ backgroundImage: "url(" + posts[2].image.url + "&fit=crop&max-w=372&max-h=192)" }}>
-      <div className="bg-white w-5/6 p-4 mx-auto">
-    <div className="text-highlight mb-1 text-xs text-center">{ posts[2].category}</div>
-        <div className="text-sm font-semibold text-center">{ posts[2].title}</div>
+    <a className={linkStyles.finalBlocks} style={{ backgroundImage: "url(" + posts[2].image.url + "&fit=crop&max-w=372&max-h=192)" }}>
+      <div>
+        <div>{ posts[2].category}</div>
+        <div>{ posts[2].title}</div>
       </div>
     </a>
     </Link>
     }
     { posts[3] && 
     <Link href={linkResolver(posts[3])}>
-    <a className={[ linkStyles.finalBlocks, "md:col-span-3 md:row-span-1 pt-8 flex justify-center items-center grid place-content-end bg-cover"].join(" ")} style={{ backgroundImage: "url(" + posts[3].image.url + "&fit=crop&max-w=372&max-h=192)" }}>
-      <div className="bg-white w-5/6 p-4 mx-auto">
-        <div className="text-highlight mb-1 text-xs text-center">{ posts[3].category}</div>
-        <div className="text-sm font-semibold text-center">{ posts[3].title}</div>
+    <a className={linkStyles.finalBlocks} style={{ backgroundImage: "url(" + posts[3].image.url + "&fit=crop&max-w=372&max-h=192)" }}>
+      <div>
+        <div>{ posts[3].category}</div>
+        <div>{ posts[3].title}</div>
       </div>
     </a>
     </Link>
