3daaa0f08b28adee19b3bcee40a56ecad13b90d7
Tim <tim@lab.co.uk>
2021-01-18 17:45:40 +0000

    Googla Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.scss'
 import "prismjs/themes/prism-tomorrow.css";
 import { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
 
 export default function App({ Component, pageProps } : AppProps) {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }
