d4ee83bc0cba835949d52dabd2143096d326cd22
Tim <tim@lab.co.uk>
2021-01-20 16:54:47 +0000

    Tag pagination

--- pages/tag/[tag]/index.tsx
+++ pages/tag/[tag]/index.tsx
@@ -11,12 +11,12 @@ import { Article, DisplayMode } from "../../../components/article"
 import { PostModel } from "../../../Models/Post"
 import { CategoryModel } from "../../../Models/Category"
 import { Client } from "../../../utils/prismicHelpers";
-import { getCategories, getCategoryIdByUid, getCategoryPosts, getTagPosts, getTags } from "../../../utils/queries"
+import { getTagPosts, getTags } from "../../../utils/queries"
 
 
 const pageSize = 3;
 
-const Category = ({page, totalPages, path, tagName, posts} : { page: number, totalPages: number, path: string, tagName: string, posts: Array<PostModel> }) => {
+const Tag = ({page, totalPages, path, tagName, posts} : { page: number, totalPages: number, path: string, tagName: string, posts: Array<PostModel> }) => {
   return (
   <Layout>
   <Head>
@@ -38,15 +38,14 @@ const Category = ({page, totalPages, path, tagName, posts} : { page: number, tot
 export const getStaticProps: GetStaticProps = async (context) => {
   const tag:string = context.params?.tag ? context.params.tag.toString() : '';
   const allTags = await getTags();
-
-  const tagName = allTags.find(x => x.toLowerCase() == tag);
+  const tagName:string = allTags.find(x => x.toLowerCase() == tag) || '';
   const posts = await getTagPosts(tagName, 1, pageSize);
 
   return {
     props: {
       page: 1,
       totalPages: posts?.totalPages,
-      path: tag,
+      path: 'tag/' + tag,
       posts: posts?.posts,
       tagName: "Tag: " + tagName
     }
@@ -64,4 +63,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-export default Category;
+export default Tag;
--- pages/tag/[tag]/page/[page].tsx
+++ pages/tag/[tag]/page/[page].tsx
@@ -10,17 +10,17 @@ import { Article, DisplayMode } from "../../../../components/article"
 import { PostModel } from "../../../../Models/Post"
 import { Client } from "../../../../utils/prismicHelpers";
 import { CategoryModel } from "../../../../Models/Category"
-import { getCategories, getCategoryIdByUid, getCategoryPosts, getPostCount, getTagPosts } from "../../../../utils/queries"
+import { getPostCount, getTagPosts, getTags } from "../../../../utils/queries"
 
 const pageSize = 3;
 
-const Category = ({page, totalPages, path, categoryName, posts} : { page: number, totalPages: number, path: string, categoryName: string, posts: Array<PostModel> }) => {
+const Tag = ({page, totalPages, path, tagName, posts} : { page: number, totalPages: number, path: string, tagName: string, posts: Array<PostModel> }) => {
   return (
   <Layout>
   <Head>
-  <title>{categoryName}</title>
+  <title>{tagName}</title>
   </Head>
-  <CategoryHeading name={categoryName}></CategoryHeading>
+  <CategoryHeading name={tagName}></CategoryHeading>
 
     <div className={styles.listing}>
     {posts.map((post) => (
@@ -34,45 +34,34 @@ const Category = ({page, totalPages, path, categoryName, posts} : { page: number
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const cat:string = context.params?.category ? context.params.category.toString() : '';
+  const tag:string = context.params?.tag ? context.params.tag.toString() : '';
   const pageNo:number = parseInt(context.params?.page ? context.params.page.toString() : '1');
-
-  var categoryId: string;
-  var categoryName: any;
-  if (cat == "blog")
-  {
-    categoryId = ""
-    categoryName = "Blog"
-  } else {
-    const category : CategoryModel = await getCategoryIdByUid(cat);
-    categoryId = category.id;
-    categoryName = category.name;
-  }
-
-  const posts = await getCategoryPosts(categoryId, pageNo, pageSize);
+  const allTags = await getTags();
+  const tagName:string = allTags.find(x => x.toLowerCase() == tag) || '';
+  const posts = await getTagPosts(tagName, pageNo, pageSize);
   
-  const categories = await getCategories();
   
   return {
     props: {
       page: pageNo,
       totalPages: posts?.totalPages,
-      path: cat,
+      path: 'tag/' + tag,
       posts: posts?.posts,
-      categoryName: categoryName
+      tagName: "Tag: " + tagName
     }
   }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {   
-  const categories = await getCategories();
+  const tags = await getTags();
 
   var routes = [];
   // add pages for category
-  for (var i = 0; i < categories.length; i++) {
-    var pages = Math.ceil(categories[i].postCount / pageSize);
+  for (var i = 0; i < tags.length; i++) {
+    const posts = await getTagPosts(tags[i], 1, pageSize);
+    var pages = posts?.totalPages || 0;
     for (var x = 0; x < pages; x++) {
-      routes.push(`/tag/${categories[i].uid}/page/${x+1}`)
+      routes.push(`/tag/${tags[i].toLowerCase()}/page/${x+1}`)
     }
   }
 
@@ -83,4 +72,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-export default Category;
+export default Tag;
