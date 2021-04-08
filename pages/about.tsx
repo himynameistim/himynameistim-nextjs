210773d189dc48484de911a379c28420a2fff973
Tim <tim@lab.co.uk>
2021-04-08 09:26:04 +0100

    Typos

--- pages/about.tsx
+++ pages/about.tsx
@@ -18,13 +18,13 @@ export default function Home() {
       <div className="container">
         <img src='/Tim Photo.jpg' className={styles.bioPhoto}></img>
         <h2>Tim Griffiths</h2>
-        <p><b>Hi my name is Tim</b>, and I am Director of Sitecore Development at Lab, a Digital Agency in the UK. I am also a developer with many years experiance specialising in ASP.NET development.</p>
+        <p><b>Hi my name is Tim</b>, and I am Director of Sitecore Development at Lab, a Digital Agency in the UK. I am also a developer with many years experience specialising in ASP.NET development.</p>
         <p>Outside of my main job I created Training Buddy a fitness tracking app for Windows Phone, which in it’s time topped the Health and Fitness category on the app store.</p>
         <h2>What's with the toy pictures?</h2>
         <p>Quite simply websites with pictures have higher engagement rates, so if you want to create an engaging website you need pictures. If the content of your website is largely code examples or articles on backend tech then this creates a bit of an issue. There’s nothing to take a photo of and there’s only so many times you can have pictures of code that have been made to look a little artsy. </p>
         <p>So, I wanted something that’s fun that I hadn’t seen on other peoples blogs and decided on toys. At times they can be relevant, like a toy screaming or lego representing building blocks, but at worst there still a fun non-offensive lighthearted picture.</p>
         <h2>Awards</h2>
-        <p>I have awarded the Sitecore Technologist MVP award 5 years running from 2017 - 2021.</p>
+        <p>I have been awarded the Sitecore Technologist MVP award 5 years running from 2017 - 2021.</p>
         <img src='/Sitecore_MVP_Technology_2021.jpg' className={styles.award}></img>
         <img src='/Sitecore_MVP_Technology_2020.jpg' className={styles.award}></img>
         <img src='/sitecore_mvp_technology_2019.jpg' className={styles.award}></img>
