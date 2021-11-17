7c8bf9dc2ef564ed177ec65cda80e5da17837194
Tim <tim@lab.co.uk>
2021-11-17 17:17:48 +0000

    Module test

--- next.config.js
+++ next.config.js
@@ -11,21 +11,28 @@ module.exports = {
 
 module.exports = {
   images: {
-    domains: ['images.prismic.io'],
+    domains: ["images.prismic.io"],
+  },
+  experimental: {
+    urlImports: [
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
   },
   async redirects() {
     return [
       {
-        source: '/category/:slug',
-        destination: '/:slug',
+        source: "/category/:slug",
+        destination: "/:slug",
         permanent: true,
-      }
-      ,
+      },
       {
-        source: '/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug',
-        destination: '/blog/:slug',
+        source: "/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug",
+        destination: "/blog/:slug",
         permanent: true,
-      }
-    ]
-  }
-}
+      },
+    ];
+  },
+};
