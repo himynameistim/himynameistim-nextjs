2d99e29d3112d65a144366ff6cb9473ad49732ea
Tim <5212530+timgriff84@users.noreply.github.com>
2022-09-30 08:33:04 +0100

    Removed old style code for get category posts

--- utils/queries.tsx
+++ utils/queries.tsx
@@ -1,6 +1,5 @@
 export { getAllPosts } from "./queries/getAllPosts"
 export { getCategories, getPostCount } from "./queries/getCategories"
-export { getCategoryPosts } from "./queries/getCategoryPosts"
 export { getLatestPosts } from "./queries/getLatestPosts"
 export { getTags } from "./queries/getTags"
 export { getTagPosts } from "./queries/getTagPosts"
