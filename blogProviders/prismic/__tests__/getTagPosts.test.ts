7bd7ed4b7bc4b7ad3c65f43b997f82c4e0cfe635
Tim <5212530+timgriff84@users.noreply.github.com>
2023-03-22 12:37:49 +0000

    Jest tests working

--- blogProviders/prismic/__tests__/getTagPosts.test.ts
+++ blogProviders/prismic/__tests__/getTagPosts.test.ts
@@ -1,14 +1,8 @@
 import "reflect-metadata";
-import { container } from "tsyringe";
-import { PrismicClient } from "../prismicClient";
-import { IGetTagPosts } from "../../blog/queries";
 import { GetTagPosts } from "..";
 
 test("getTagPosts returns more than 0 posts", async () => {
-  const pc = new PrismicClient("himynameistim");
-  const gtp: IGetTagPosts = new GetTagPosts(pc);
-
-  const res = await gtp.getTagPosts("Sitecore", 1, 1);
+  const res = await GetTagPosts("Sitecore", 1, 1);
 
   expect(res.posts.length).toBeGreaterThan(0);
 });
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
     "^.+\\.(js)$": "babel-jest",
   },
-  transformIgnorePatterns: [
-  ],
+  transformIgnorePatterns: [],
+  setupFiles: ["dotenv/config"],
 };
